59fd9576d09023da94cffe28715a7620145f906c
papchenko <nikopapchenko@gmail.com>
2025-02-13 09:18:30 +0200

    ff3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://papchenko.github.io',
-    base: 'astrosite',
+    // site: 'https://papchenko.github.io',
+    // base: 'astrosite',
+    site: 'https://astrosite-4v23.vercel.app/',
+    
+
+    output: 'server',
+    adapter: vercel(),
 });
